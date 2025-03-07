09e64e064c50819a15fe3f630cf75a31c18c6bf4
warrior2004 <harshit.honey003@gmail.com>
2025-03-07 13:09:36 +0530

    Updated project structure

--- template.py
+++ template.py
@@ -6,7 +6,6 @@ logging.basicConfig(level=logging.INFO)
 
 project_name = "AI_Agentic_Chatbot"
 
-list_of_folders = ["vectorstore","data"]
 list_of_files = [
     f"src/__init__.py",
     f"src/ai_agent.py",
@@ -14,19 +13,11 @@ list_of_files = [
     f"src/backend.py",
     f"src/logger.py",
     f"src/exception.py",
-    f"app.py",
     f"setup.py",
     "Dockerfile",
     ".dockerignore",
     "requirements.txt"
 ]
-
-for folder in list_of_folders:
-    if not os.path.exists(folder):
-        os.makedirs(folder, exist_ok=True)
-        logging.info(f"Folder '{folder}' created successfully.")
-    else:
-        logging.info(f"Folder '{folder}' already exists.")
 
 for filepath in list_of_files:
     filepath = Path(filepath)
