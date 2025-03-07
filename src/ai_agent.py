cd0cf654d6bf2742bbf89d1c6d71e03fe3a6668e
warrior2004 <harshit.honey003@gmail.com>
2025-03-07 11:35:42 +0530

    Updated backend.py file

--- src/ai_agent.py
+++ src/ai_agent.py
@@ -7,14 +7,21 @@ from langgraph.prebuilt import create_react_agent
 from langchain_core.messages.ai import AIMessage
 from src.logger import logging
 from src.exception import CustomException
+from langsmith import traceable
 import sys
 
 load_dotenv()
+
 
 # Step 1: Load API Key
 GROQ_API_KEY = os.environ.get("GROQ_API_KEY")
 TAVILY_API_KEY = os.environ.get("TAVILY_API_KEY")
 OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
+LANGCHAIN_API_KEY = os.environ.get("LANGCHAIN_API_KEY")
+
+# Enable LangSmith Tracing
+os.environ["LANGCHAIN_TRACING_V2"] = "true"
+os.environ["LANGCHAIN_API_KEY"] = LANGCHAIN_API_KEY
 
 if not GROQ_API_KEY or not TAVILY_API_KEY or not OPENAI_API_KEY:
     logging.error("API keys are missing. Please check your environment variables.")
@@ -23,21 +30,26 @@ if not GROQ_API_KEY or not TAVILY_API_KEY or not OPENAI_API_KEY:
 openai_llm=ChatOpenAI(model="gpt-4o-mini")
 groq_llm=ChatGroq(model="llama-3.3-70b-versatile")
 
-search_tool=TavilySearchResults(max_results=2)
-logging.info("Getting query from AI agent")
-try:
-    system_prompt = "Act as an AI chatbot who is smart and friendly"
-    agent=create_react_agent(
-        model=groq_llm,
-        tools=[search_tool],
-        state_modifier=system_prompt
-    )
-    query = "Tell me about the trends in cryptocurrency"
-    state={"messages": query}
-    response=agent.invoke(state)
-    messages = response.get("messages")
-    ai_messages = [message.content for message in messages if isinstance(message, AIMessage)]
-    logging.info("Response generated sucessfully")
-    print(ai_messages[-1])
-except Exception as e:
-    raise CustomException(e, sys)
+
+def get_response_from_ai_agent(llm_id, query, allow_search, system_prompt, provider):
+    logging.info("Getting query from AI agent")
+    try:
+        if provider == "groq":
+            llm = ChatGroq(model=llm_id)
+        elif provider == "openai":
+            llm = ChatOpenAI(model=llm_id)
+        system_prompt = "Act as an AI chatbot who is smart and friendly"
+        tools=[TavilySearchResults(max_results=2)] if allow_search else []
+        agent=create_react_agent(
+            model=llm,
+            tools=tools,
+            state_modifier=system_prompt
+        )
+        state={"messages": query}
+        response=agent.invoke(state)
+        messages = response.get("messages")
+        ai_messages = [message.content for message in messages if isinstance(message, AIMessage)]
+        logging.info("Response generated sucessfully")
+        return ai_messages[-1]
+    except Exception as e:
+        raise CustomException(e, sys)
---
+++ src/backend.py
@@ -0,0 +1,39 @@
+from pydantic import BaseModel
+from typing import List
+from fastapi import FastAPI
+from src.ai_agent import get_response_from_ai_agent
+import uvicorn
+
+#Step1: Setup Pydantic Model (Schema Validation)
+class RequestState(BaseModel):
+    model_name: str
+    model_provider: str
+    system_prompt: str
+    messages: List[str]
+    allow_search: bool
+
+#Step2: Setup AI Agent from FrontEnd Request
+app = FastAPI(title="AI Agent API")
+ALLOWED_MODEL_NAMES=["llama3-70b-8192", "mixtral-8x7b-32768", "llama-3.3-70b-versatile", "gpt-4o-mini"]
+
+@app.post("/chat")
+def chat_endpoint(request_state: RequestState):
+    """
+    API Endpoint to interact with the Chatbot using LangGraph and search tools.
+    It dynamically selects the model specified in the request
+    """
+    if request_state.model_name not in ALLOWED_MODEL_NAMES:
+        return {"error": "Invalid model name"}
+    
+    llm_id = request_state.model_name
+    query = request_state.messages
+    allow_search = request_state.allow_search
+    system_prompt = request_state.system_prompt
+    provider = request_state.model_provider
+
+    response = get_response_from_ai_agent(llm_id, query, allow_search, system_prompt, provider)
+    return response
+
+#Step3: Run app & Explore Swagger UI Docs
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=5000)
